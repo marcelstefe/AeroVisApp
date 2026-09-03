af9512c3f1338a7a01fec66509429e115320236c
marcelstefe <marcelstefe20081@gmail.com>
2026-09-03 22:54:42 +0200

    New Commit

--- AeroVisApp/App.xaml.cs
+++ AeroVisApp/App.xaml.cs
@@ -1,5 +1,5 @@
-﻿using System.Configuration;
-using System.Data;
+using System;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace AeroVisApp;
@@ -9,4 +9,15 @@ namespace AeroVisApp;
 /// </summary>
 public partial class App : Application
 {
+    private async void Application_Startup(object sender, StartupEventArgs e)
+    {
+        var splash = new SplashWindow();
+        splash.Show();
+
+        await Task.Delay(TimeSpan.FromSeconds(2));
+
+        var main = new MainWindow();
+        main.Show();
+        splash.Close();
+    }
 }
